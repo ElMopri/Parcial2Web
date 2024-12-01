0e38dc6c448b7f8d1692c93e1db9aa4c2188aeaa
ElMopri <alopeztriana1@gmail.com>
2024-12-01 16:48:38 -0500

    nose que cambios hay

--- src/main/java/co/edu/ufps/repositories/ProjectAssignmentRepository.java
+++ src/main/java/co/edu/ufps/repositories/ProjectAssignmentRepository.java
@@ -1,16 +1,9 @@
 package co.edu.ufps.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import co.edu.ufps.entities.Project;
 import co.edu.ufps.entities.ProjectAssignment;
 
 public interface ProjectAssignmentRepository extends JpaRepository<ProjectAssignment,Integer>{
-	
-	//public List<ProjectAssignment> findAllByProject_id(Project project_id);
-	
-	//public boolean existsByProject_id(Project project_id);
 	
 }
